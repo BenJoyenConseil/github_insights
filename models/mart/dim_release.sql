3b6a011476fec62c4a6aeb9ee9db5b9a57cd1b00
BenJoyenConseil <benjamin.joyen@decathlon.com>
2024-11-17 18:39:22 +0100

    enhance release with fix detection

--- models/mart/dim_release.sql
+++ models/mart/dim_release.sql
@@ -2,5 +2,6 @@ select
     d_date, 
     version, 
     author, 
-    case when('fix' in body or 'Fix' in body) then TRUE else FALSE end has_fix
+    case when(regexp_matches(body, 'fix', 'i')) then TRUE else FALSE end has_fix,
+    body
 from {{ ref('stg_releases')}}
