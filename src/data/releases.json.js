29df372f108b1ee9afb27a810bd49471a4a30a30
BenJoyenConseil <benjamin.joyen@decathlon.com>
2024-11-19 20:03:06 +0100

    variabilization of contributors and releases queries

--- src/data/releases.json.js
+++ src/data/releases.json.js
@@ -1,13 +1,19 @@
 import { Octokit } from "octokit";
 import dotenv from 'dotenv'; 
+
 dotenv.config();
+
+const gh_token = process.env.GITHUB_TOKEN
+const gh_organization = process.env.GITHUB_ORG
+const gh_repo = process.env.GITHUB_REPO
+
 const octokit = new Octokit({
-    auth: process.env.GITHUB_TOKEN
+    auth: gh_token
 })
 
 const data = await octokit.paginate("GET /repos/{owner}/{repo}/releases", {
-  owner: "dktunited",
-  repo: "insight-all-sources",
+  owner: gh_organization,
+  repo: gh_repo,
   per_page: 100,
   headers: {
     "X-GitHub-Api-Version": "2022-11-28",
