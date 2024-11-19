9527c1f1111a958eb5170f2d209f30916f2465e9
BenJoyenConseil <benjamin.joyen@decathlon.com>
2024-11-19 19:07:49 +0100

    get Github objects using CURL

---
+++ src/data/insight_all_sources_commits.json.sh
@@ -0,0 +1,8 @@
+#/usr/bin/env sh
+source .env
+
+curl -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/dktunited/insight-all-sources/stats/participation
