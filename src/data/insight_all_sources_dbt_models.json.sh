4ce3f430b04b4dae36201ac6deefd99791a920ac
BenJoyenConseil <benjamin.joyen@decathlon.com>
2024-11-19 20:00:16 +0100

    archive old scripts

--- src/data/insight_all_sources_dbt_models.json.sh
+++
@@ -1,9 +0,0 @@
-#/usr/bin/env sh
-
-# git clone --single-branch -b main https://github.com/dktunited/insight-all-sources.git temp_allsources
-# cd temp_allsources
-# virtualenv venv 
-# pip install -r requirements.txt
-# dbt deps
-# dbt ls --target dev
-# cat target/manifest.json
--- src/data/insight_all_sources_releases.json.sh
+++
@@ -1,8 +0,0 @@
-#/usr/bin/env sh
-source .env
-
-curl -L \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/dktunited/insight-all-sources/releases?per_page=100
