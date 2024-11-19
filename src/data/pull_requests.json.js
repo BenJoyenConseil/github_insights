47d2d081ef54c3937d791e83b6a8c660e6fae098
BenJoyenConseil <benjamin.joyen@decathlon.com>
2024-11-19 19:06:48 +0100

    request PR using graphql

--- src/data/pull_requests.json.js
+++ src/data/pull_requests.json.js
@@ -1,17 +1,66 @@
-import { Octokit } from "octokit";
 import dotenv from 'dotenv'; 
+import { Octokit } from "@octokit/core";
+import { paginateGraphQL } from "@octokit/plugin-paginate-graphql";
+import { request } from "@octokit/request";
+
 dotenv.config();
-const octokit = new Octokit({
-    auth: process.env.GITHUB_TOKEN
-})
+const token = process.env.GITHUB_TOKEN
 
-const data = await octokit.paginate("GET /repos/{owner}/{repo}/pulls", {
-  owner: "dktunited",
-  repo: "insight-all-sources",
-  per_page: 100,
-  headers: {
-    "X-GitHub-Api-Version": "2022-11-28",
+const MyOctokit = Octokit.plugin(paginateGraphQL);
+const octokit = new MyOctokit({ auth: token });
+
+const { repository } = await octokit.graphql.paginate(
+  `query paginate($cursor: String, $organization: String!, $repo: String!) {
+    repository(owner: $organization, name: $repo) {
+      pullRequests(baseRefName: "main", first: 10, after: $cursor) {
+        nodes {
+          createdAt
+          mergedAt
+          closedAt
+          number
+          title
+          additions
+          deletions
+          changedFiles
+          bodyText
+          totalCommentsCount
+          merged
+          closed
+          isDraft
+          state
+          commits(first: 10) {
+            edges {
+              node {
+                commit {
+                  abbreviatedOid
+                  committedDate
+                  author {
+                    email
+                    name
+                  }
+                  message
+                  additions
+                  deletions
+                  changedFilesIfAvailable
+                }
+              }
+            }   
+            pageInfo {
+              hasNextPage
+              endCursor
+            }   
+          }    
+        }
+        pageInfo {
+          hasNextPage
+          endCursor
+        }
+      }
+    }
+  }`,{
+      organization: "dktunited",
+      repo: "insight-all-sources",
   },
-});
+);
 
-console.log(JSON.stringify(data))
+console.log(JSON.stringify(repository.pullRequests.nodes))
