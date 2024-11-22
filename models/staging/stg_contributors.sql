83272e221329fedbc97e3c8ed9d2961c648e35b2
BenJoyenConseil <benjamin.joyen@decathlon.com>
2024-11-22 01:38:11 +0100

    dbt : remove contributions

--- models/staging/stg_contributors.sql
+++
@@ -1,8 +0,0 @@
-select 
-    author.login, 
-    to_timestamp(unnest(weeks).w)::date as d_date, 
-    unnest(weeks).a as additions, 
-    unnest(weeks).d as deletions, 
-    unnest(weeks).c as commits 
-from 
-    read_json('src/data/raw/contributions*.json')
